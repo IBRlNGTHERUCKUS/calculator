f2322343fe675bcc2b9ffc38282ecb8f03d53198
Joseph C <josephalexcobian@gmail.com>
2023-09-25 23:14:51 -0700

    Add functionality to '=' sign

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const buttons = document.querySelectorAll('button');
 for (let button of buttons) {
     button.addEventListener('click', handleButtons);
 }
+const output = document.querySelector('.output');
 let opr1 = 0, opr2, operator;
+
+function compute(operand1, operator, operand2) {
+    if (operator == '+') {return operand1 + operand2;}
+    else if (operator == '-') {return operand1 - operand2}
+    else if (operator == '*') {return operand1 * operand2}
+    else if (operator == '/') {return operand2 != 0 ? operand1 / operand2 : "error"}
+}
+
+function update(out) {
+    output.textContent = out;
+}
 
 function handleButtons(event) {
     let buttonPressed = event.target;
@@ -13,22 +25,27 @@ function handleButtons(event) {
         if (opr1 && operator && opr2) {
             opr1 = compute(opr1, operator, opr2);
             opr2 = null;
-            update(opr1);
+           update(opr1);
         }
+        else if (operator = '=') {operator = null}
         operator = buttonPressed.textContent;
     }
     else if (buttonPressed.className == 'number') {
         if (opr1 == null && operator == null) {
             opr1 = +buttonPressed.textContent;
+            update(opr1);
         }
         else if (opr1 != null && operator == null) {
             opr1 = opr1 * 10 + +buttonPressed.textContent;
+            update(opr1);
         }
         else if (opr2 == null) {
             opr2 = +buttonPressed.textContent;
+            update(opr2);
         }
         else {
             opr2 = opr2 * 10 + +buttonPressed.textContent;
+            update(opr2);
         }
     }
 }
